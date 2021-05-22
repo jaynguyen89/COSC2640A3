47038a41da4d66f104da9c5545b293f796351201
Jay Nguyen <nguyen.le.kim.phuc@gmail.com>
2021-05-22 14:54:42 +1000

    Deployed database and lambda apps.

--- Database/COSC2640A3/publish/arbitrary-queries.sql
+++ Database/COSC2640A3/publish/arbitrary-queries.sql
@@ -5,3 +5,5 @@ SELECT * FROM [dbo].Teacher;
 SELECT * FROM [dbo].Classroom;
 SELECT * FROM [dbo].Enrolment;
 SELECT * FROM [dbo].Invoice;
+
+update Invoice set IsPaid = '0', PaymentMethod = null, PaymentId = null, TransactionId = null, ChargeId = null, PaymentStatus = null, PaidOn = null;
